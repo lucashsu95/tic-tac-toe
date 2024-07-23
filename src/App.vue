e63457cbf1cd379c96512a53447390465ea094bb
lucashsu95 <lucashsu95@gmail.com>
2024-07-23 14:27:39 +0800

    Update App.vue and Add describute

--- src/App.vue
+++ src/App.vue
@@ -11,9 +11,7 @@ const userClick = (e) => {
 
   if (robotMod.value) {
     e.target.textContent = 'X'
-    setTimeout(() => {
-      robotClick()
-    }, 200)
+    setTimeout(robotClick, 200)
   } else {
     e.target.textContent = flag ? 'X' : 'O'
     flag = !flag
@@ -24,8 +22,7 @@ const userClick = (e) => {
 
 const robotClick = () => {
   if (isWin.value) return
-  const divs = [...document.querySelectorAll('.box > div')]
-  const can = divs.filter((x) => x.textContent === '')
+  const can = divs.value.filter((x) => x.textContent === '')
   const rand = Math.floor(Math.random() * can.length)
 
   can[rand].textContent = 'O'
@@ -38,18 +35,17 @@ const checkOrder = (key) => {
 
   if (clickOrder.value.length > 6) {
     const remove = clickOrder.value.shift()
-    document.querySelector(`[data-key='${remove}']`).textContent = ''
+    divs.value[remove].textContent = ''
   }
 }
 
 const wins = ['123', '456', '789', '147', '258', '369', '159', '357']
 const checkWin = () => {
-  const divs = [...document.querySelectorAll('.box > div')]
-  const o = divs
+  const o = divs.value
     .filter((x) => x.textContent === 'O')
     .map((x) => x.dataset.key)
     .join('')
-  const x = divs
+  const x = divs.value
     .filter((x) => x.textContent === 'X')
     .map((x) => x.dataset.key)
     .join('')
@@ -61,40 +57,61 @@ const checkWin = () => {
   }
 }
 
+const divs = ref([]);
 const resetGame = () => {
   isWin.value = null
-  clickOrder.value = []
-  ;[...document.querySelectorAll('.box > div')].map((x) => (x.textContent = ''))
+  clickOrder.value = [];
+  divs.value.map(x => x.textContent = '');
 }
 </script>
 
 <template>
   <h1>圈圈差差</h1>
+  <h3>畫到第四次時會把第一次畫的地方給清空，增加了思考的空間</h3>
+  <div>再說明一次...這不是Bug</div>
   <label for="robotMod">邊緣人模式</label><input type="checkbox" v-model="robotMod" id="robotMod" />
   <h2 v-show="isWin">{{ isWin }} Win!</h2>
   <div class="box">
-    <div v-for="i in 9" :key="i" @click="userClick" :data-key="i"></div>
+    <div v-for="i in 9" :key="i" ref="divs" @click="userClick" :data-key="i - 1"></div>
   </div>
   <button class="btn btn-primary mt-3" @click="resetGame">Reset</button>
 </template>
 
 <style>
+h1 {
+  margin-bottom: 10px;
+}
+
+h3 {
+  margin-top: 0;
+  margin-bottom: 5px;
+}
+
 .box {
   display: grid;
   width: 400px;
   height: 400px;
   grid-template-columns: repeat(3, 1fr);
   grid-template-rows: repeat(3, 1fr);
+}
 
-  & > div {
-    font-size: 48px;
-    border: 1px solid #ccc;
-    margin: 5px;
-    border-radius: 0.375rem;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    transition: 0.5s;
-  }
+.box>div {
+  font-size: 48px;
+  border: 1px solid #ccc;
+  margin: 5px;
+  border-radius: 0.375rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: 0.5s;
+}
+
+button{
+  border-radius: 45px;
+  background: #121212;
+  color:#39f;
+  padding: .5rem 3rem;
+  border:4px solid #d6d6d7;
+  cursor: pointer;
 }
 </style>
